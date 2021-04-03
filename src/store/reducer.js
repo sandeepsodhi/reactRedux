7d899a40dd8effd17f9ebbab099681e2cf8aace8
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-04-03 01:53:22 -0400

    passing and retrieving data with action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,7 +5,22 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     if(action.type === 'INCREMENT'){
         return {
-            counter: state.counter +1
+            counter: state.counter + 1
+        }
+    }
+    if(action.type === 'DECREMENT'){
+        return {
+            counter: state.counter - 1
+        }
+    }
+    if(action.type === 'ADD'){
+        return {
+            counter: state.counter + action.val
+        }
+    }
+    if(action.type === 'SUBTRACT'){
+        return {
+            counter: state.counter - action.val
         }
     }
     return state;
