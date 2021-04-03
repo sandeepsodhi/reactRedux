d7025bce7569d30f58c4698f939172f29b1d5bd3
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-04-03 00:48:01 -0400

    dispatching action

--- redux-basics.js
+++ redux-basics.js
@@ -7,6 +7,18 @@ const initialState = {
 
 //reducer
 const rootReducer = (state = initialState, action) => {
+    if(action.type === 'INC_COUNTER'){
+        return{
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+    if(action.type === 'ADD_COUNTER'){
+        return{
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
     return state;
 };
 
@@ -15,5 +27,10 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 ///dispatching action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
+
+
 
 //subscription
