ca3327f8d876cb2719622c658a401850bed6ba33
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-04-04 13:23:06 -0400

    installing redux thunk and handling asynchronous code

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -27,12 +27,19 @@ export const subtract = (value) => {
         val: value
     }
 };
-export const storeResult = (res) => {
+export const saveResult = (res) => {
     return {
         type: STORE_RESULT,
         result: res
     }
-};
+}
+export const storeResult = (res) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(saveResult(res));
+        }, 2000);
+    }
+}; 
 export const deleteResult = (resElId) => {
     return {
         type: DELETE_RESULT,
