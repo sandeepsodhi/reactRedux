29dc41c0181e8351511fdc1dedbd5821953092e0
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-04-10 18:03:59 -0400

    adding a utility function

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility';
 
 const initialState = {
     counter: 0
@@ -7,24 +8,13 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch(action.type){
         case actionTypes.INCREMENT:
-            const newState = Object.assign({}, state); //not doing in old state, copying new state immutably and return it, it will be new object.
-            newState.counter = state.counter + 1;
-            return newState;
+            return updateObject(state, {counter: state.counter + 1});
         case actionTypes.DECREMENT:
-            return {
-                ...state, //another version above logic
-                counter: state.counter - 1
-            }
+            return updateObject(state, {counter: state.counter - 1});
         case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.val
-            }
+            return updateObject(state, {counter: state.counter + action.val});
         case actionTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.val
-            }
+            return updateObject(state, {counter: state.counter - action.val});
         default:
             return state;
     }
